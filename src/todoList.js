86edfafa31cf9c0635b24d97ad5f68338084359d
pavlosantoniou13 <dinant11133@gmail.com>
2023-01-23 23:30:41 +0200

    A

--- src/todoList.js
+++ src/todoList.js
@@ -7,7 +7,7 @@ function Todo (title, description, date) {
     this.title = title
     this.description = description
     this.date = date
-
+    this.id = Date.now()
     
 }
 
@@ -38,6 +38,14 @@ dueDate.type = "date"
 const sumbitBtn = document.createElement("button")
 sumbitBtn.type = "submit"
 
+//3const deleteBtn = document.createElement("button")
+
+//const formBoxDataList = document.createElement("div")
+//const DataListLabel = document.createElement("label")
+//const dataList = document.createElement("sele")
+//dataList.option = "malakas"
+
+
 content.appendChild(form).className = "form";
 form.appendChild(formBoxTitle)
 formBoxTitle.appendChild(labelTitle)
@@ -48,7 +56,12 @@ formBoxDescription.appendChild(description).className = "description"
 form.appendChild(formBoxDate)
 formBoxDate.appendChild(dueDateLabel)
 formBoxDate.appendChild(dueDate).className = "date"
+//form.appendChild(formBoxDataList)
+//formBoxDataList.appendChild(DataListLabel)
+//formBoxDataList.appendChild(dataList)
+
 form.appendChild(sumbitBtn).className = "submit"
+//1content.appendChild(deleteBtn).className = "delete"
 
 
 
@@ -56,14 +69,20 @@ form.appendChild(sumbitBtn).className = "submit"
 labelTitle.textContent = "Title:"
 labelDescription.textContent = "Description:"
 sumbitBtn.textContent = "Submit"
+//DataListLabel.textContent = "Urgency:"
+//2deleteBtn.textContent = "Delete"
 
 
 
 sumbitBtn.addEventListener('click', makeTodo)
 
+//4deleteBtn.addEventListener("click", deleteTodo)
+
 }
 
 function makeTodo(ev) {
+    const deleteBtn = document.createElement("button")
+    content.appendChild(deleteBtn).className = "delete"
 
     const title = document.querySelector(".title")
     const description = document.querySelector(".description")
@@ -73,12 +92,28 @@ function makeTodo(ev) {
     console.log(ev)
 
     const todo = new Todo(title.value, description.value, date.value )
+    deleteBtn.value = todo.id
         
     todoStorage.push(todo)
     console.log(todoStorage)
     
-   
-    
+    ev.target.closest("form").remove() 
+    deleteBtn.textContent = "Delete"
+    deleteBtn.addEventListener("click", deleteTodo)
+}
+
+
+function deleteTodo(ev){
+    const id = ev.target.value;
+    todoStorage = todoStorage.filter(
+       todo => {
+       if(todo.id == id) {
+        return false
+       } else {
+        return true
+       }
+    })
+    //ev.target.closest('.card').remove();
 }
 
 export default formCreate
