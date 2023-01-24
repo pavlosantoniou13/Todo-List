b6e42ced109e814a1d8e08f12e343b0ac1fb39c1
pavlosantoniou13 <dinant11133@gmail.com>
2023-01-24 20:11:54 +0200

    A

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require = require('path')
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
